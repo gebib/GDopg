419215e30c2a9c314d565a3dbc3b2ebab999d0cb
gebib <gebi9@hotmail.com>
2022-01-31 22:01:21 +0100

    Oppg2 spring boot API initial commit

---
+++ oppg2/src/main/java/com/example/oppg2/Oppg2Application.java
@@ -0,0 +1,13 @@
+package com.example.oppg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oppg2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oppg2Application.class, args);
+    }
+
+}
---
+++ oppg2/src/test/java/com/example/oppg2/Oppg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oppg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oppg2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
